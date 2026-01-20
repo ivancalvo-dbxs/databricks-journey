07ff8e84633db2d9a1d504cbce2a71ef557853a8
ivancalvo-dbxs <ivan.calvo@databricks.com>
2026-01-20 12:52:03 -0600

    cheatsheet added

--- docs/starter-journey/docusaurus.config.ts
+++ docs/starter-journey/docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ivancalvo-dbxs', // Usually your GitHub org/user name.
   projectName: 'starter-journey', // Usually your repo name.
-  deploymentBranch: 'la-starter-journey-page',
+  deploymentBranch: 'github-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
